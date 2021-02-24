b0462dad6a3cee7d60febc8179f33a666ed1b639
Jose Storopoli <thestoropoli@gmail.com>
2021-02-24 09:20:41 -0300

    changed binomial_nasa.jl

--- Turing/StatisticalRethinking/binomial_nasa.jl
+++ Turing/StatisticalRethinking/binomial_nasa.jl
@@ -1,12 +1,10 @@
 # http://sherrytowers.com/2018/03/07/logistic-binomial-regression/
-using Turing, CSV, DataFrames, LazyArrays, Plots, Loess
-using StatsFuns:log
+using Turing, CSV, DataFrames, LazyArrays, Plots
 
 df = CSV.read("Turing/StatisticalRethinking/oring.csv", DataFrame; header=2)
 
 # there are only 6 O-rings in total
 df.num_orings = 6 .- df.num_failure
-df.temp = (df.temp .- 32) ./ 1.8
 
 @model binomial_model(num_orings, temp, num_failure) = begin
     α ~ TDist(3)
@@ -18,8 +16,8 @@ model = binomial_model(df.num_orings, df.temp, df.num_failure)
 
 chn = sample(model, NUTS(1_000, 0.65), MCMCThreads(), 2_000, 4)
 
-temps = (10:80 .- 32) ./ 1.8
-avg, beta = mean(chn)[:, :mean]
+temps = 10:80
+avg, beta = median(chn)[:, :mean]
 y = avg .+ exp.(beta .* temps)
 
 
