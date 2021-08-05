f0c2ecdbd131bd79e38a5c52ae1367497b1d36bc
Jose Storopoli <thestoropoli@gmail.com>
2021-08-05 10:28:11 -0300

    gradient_descent with slider

--- Plots/Makie_AoG/gradient_descent.jl
+++ Plots/Makie_AoG/gradient_descent.jl
@@ -5,14 +5,15 @@ Pkg.activate(mktempdir())
 Pkg.add([
     "CairoMakie",
     "WGLMakie",
+    "GLMakie",
     "ForwardDiff"
 ])
 
 # using CairoMakie # No interactivity
-using WGLMakie   # Interactivity
+using GLMakie   # Interactivity
 using ForwardDiff
 
-function ascend(
+function descend(
     Δ::Real,
     x0::Real,
     y0::Real;
@@ -41,41 +42,55 @@ f(x::Array{T, 1} where T <: Real)  = f(x[1], x[2])
 ∇f(x::Real, y::Real) = ForwardDiff.gradient(f, [x, y])
 ∇f(x::Array{<:AbstractFloat, 1}) = ForwardDiff.gradient(f, x)
 
-fig = Figure()
+fig = Figure(resolution=(800, 800))
 
 # 2-D
-# ax1 = Axis(fig[1,1])
+ax1 = Axis(fig[1, 1])
 # 3-D
-ax1 = Axis3(fig[1,1])
-
+ax2 = Axis3(fig[2, 1])
 
 # different plots you can see of f and ∇f
-# remove comment to display
-
 xa = LinRange(-5, 5, 500)
 ya = LinRange(-5, 5, 500)
-za = [f(x, y) for x ∈ xa, y ∈ ya]
+za = .-[f(x, y) for x ∈ xa, y ∈ ya]
 # ∇za = [∇f(x, y) for x ∈ xa, y ∈ ya]
 
-fsurf = surface!(ax1, xa, ya, za, shading = false)
-# ∇fsurf = surface!(ax1, xa, ya, ∇za, shading = false)
+plotobj2d = contour3d!(ax1, xa, ya, za; shading=false, linewidth=3, levels=20)
+plotobj3d = surface!(ax2, xa, ya, za; shading=false)
+# fsurf = surface!(ax1, xa, ya, za; shading = false)
+# ∇fsurf = surface!(ax1, xa, ya, ∇za; shading = false)
+# fcont = contour!(ax1, xa, ya, za; levels = 20, linewidth = 3)
+# ∇fcont = contour!(ax1, xa, ya, ∇za; levels = 20, linewidth = 3)
+# fheat = heatmap!(ax1, xa, ya, za)
+# ∇fheat = heatmap!(ax1, xa, ya, ∇za)
+# fcont3 = contour3d!(ax1, xa, ya, za; levels = 20, linewidth = 3)
+# ∇fcont3 = contour3d!(ax1, xa, ya, ∇za; levels = 20, linewidth = 3)
 
-fcont = contour!(ax1, xa, ya, za, levels = 20, linewidth = 3)
-# ∇fcont = contour!(ax1, xa, ya, ∇za, levels = 20, linewidth = 3);
+slider_η = labelslider!(
+    fig,
+    "η",
+    0.01:0.001:2.0;
+    width = 350,
+    tellwidth=false)
 
-fheat = heatmap!(ax1, xa, ya, za);
-# ∇fheat = heatmap!(ax1, xa, ya, ∇za);
+# slider_iter = labelslider!(
+#     fig,
+#     "Iterações",
+#     10:10:100)
 
-fcont3 = contour3d!(ax1, xa, ya, za, levels = 20, linewidth = 3)
-# ∇fcont3 = contour3d!(ax1, xa, ya, ∇za, levels = 20, linewidth = 3);
-
-sl_x = MakieLayout.Slider(fig[2, 1], range=0.01:0.01:4, startvalue=0.1)
-x0 = Node(0f0)
-y0 = Node(0f0)
-coords = @lift(ascend($(sl_x.value), $x0, $y0))
+fig[3, 1]= slider_η.layout
+# fig[4, 1]= slider_iter.layout
+x0 = Node(0.0)
+y0 = Node(0.0)
+coords = @lift(descend($(slider_η.slider.value), $x0, $y0))
 xs = lift(x -> x[:, 1], coords)
 ys = lift(x -> x[:, 2], coords)
-zs = lift((x, y) -> f.(x, y), xs, ys) # for three dimensional plots
-lines!(ax1, xs, ys, zs, color = :black, linewidth = 10)
+zs = lift((x, y) -> .-f.(x, y), xs, ys) # for three dimensional plots
+scatterlines!(ax1, xs, ys, zs, color = :red, linewidth = 5)
+scatterlines!(ax2, xs, ys, zs, color = :red, linewidth = 5)
+hidedecorations!(ax1)
+hidedecorations!(ax2)
+hidespines!(ax1)
+hidespines!(ax2)
 
 fig
